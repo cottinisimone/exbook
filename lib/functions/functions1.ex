7b899c673d6c8dd4566b99bfad1e85336bbf423c
Simone Cottini <cottini.simone@gmail.com>
2018-07-09 10:50:51 +0200

    Functions-5. Examples rewritten as moduledoc

--- lib/functions/functions1.ex
+++ lib/functions/functions1.ex
@@ -1,10 +1,39 @@
-defmodule Function1 do
+defmodule Functions1 do
     
-    @list_concat = fn a, b -> a ++ b end
-    @list_concat2 = fn [a, b], [c, d] -> [a, b, c, d] end
+    @moduledoc """
+    
+    Functions1 -> Page 70
 
-    @sum = fn a, b, c -> a + b + c end
+    iex> list_concat = fn a, b -> a ++ b end
+    #Function<12.99386804/2 in :erl_eval.expr/5>
 
-    @pair_tuple_to_list = fn {a, b} -> [a, b] end
+    iex> list_concat.([:a, :b], [:c, :d])
+    [:a, :b, :c, :d]
+
+    ---
+
+    iex> list_concat2 = fn [a, b], [c, d] -> [a, b, c, d] end
+    #Function<12.99386804/2 in :erl_eval.expr/5>
+
+    iex> list_concat2.([:a, :b], [:c, :d])
+    [:a, :b, :c, :d]
+
+    ---
+
+    iex> sum = fn a, b, c -> a + b + c end
+    #Function<18.99386804/3 in :erl_eval.expr/5>
+
+    iex> sum.(1, 2, 3)
+    6
+
+    ---
+
+    iex> pair_tuple_to_list = fn {a, b} -> [a, b] end
+    #Function<6.99386804/1 in :erl_eval.expr/5>
+
+    iex> pair_tuple_to_list.( { 1234, 5678 } )
+    [1234, 5678]
+
+    """
 
 end
--- lib/functions/functions3.ex
+++ lib/functions/functions3.ex
@@ -1,11 +1,38 @@
 defmodule Functions3 do
-    @remfb = fn x -> @fizzbuzz.(rem(x, 3), rem(x, 5), x) end
+   @moduledoc """
 
-    # remfb.(10) ==> Buzz
-    # remfb.(11) ==> 11
-    # remfb.(12) ==> Fizz
-    # remfb.(13) ==> 13
-    # remfb.(14) ==> 14
-    # remfb.(15) ==> FizzBuzz
-    # remfb.(16) ==> 16
+   Functions3 -> Page 73
+
+    iex> fizzbuzz = fn 
+            0, 0, _ -> "FizzBuzz"
+            0, _, _ -> "Fizz"
+            _, 0, _ -> "Buzz"
+            _, _, c -> c
+        end
+    #Function<18.99386804/3 in :erl_eval.expr/5>
+
+    iex >reminder_fizz_buzz = fn x -> fizzbuzz.(rem(x, 3), rem(x, 5), x) end
+    #Function<6.99386804/1 in :erl_eval.expr/5>
+
+    iex> remfb.(10)
+    "Buzz"
+    
+    iex> remfb.(11)
+    11
+
+    iex> remfb.(12)
+    "Fizz"
+
+    iex> remfb.(13)
+    13
+
+    iex> remfb.(14)
+    14
+
+    iex> remfb.(15)
+    "FizzBuzz"
+
+    iex> remfb.(16)
+    16
+    """
 end

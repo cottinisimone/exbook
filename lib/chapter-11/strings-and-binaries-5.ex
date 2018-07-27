f05db0806dc71d49ff0ef0c7120e27350ffbdf87
Simone Cottini <cottini.simone@gmail.com>
2018-07-27 17:37:51 +0200

    Ex 5 and 6 strings and binaries

--- lib/chapter-11/strings-and-binaries-5.ex
+++ lib/chapter-11/strings-and-binaries-5.ex
@@ -1,7 +1,39 @@
 # elixir lib/chapter-11/strings-and-binaries-5.ex
 
 defmodule Table do
-  def center()
+  # Using strings
+  def center(list), do: _align(list, _max_size(list, 0))
+
+  defp _align([], size), do: String.pad_leading("", size, "-")
+
+  defp _align([head | tail], size),
+    do:
+      head
+      |> String.pad_trailing(_calc_trail(String.length(head), size))
+      |> String.pad_leading(size)
+      |> (fn str -> str <> "\n" <> _align(tail, size) end).()
+
+  defp _max_size([], max), do: max
+
+  defp _max_size([head | tail], max) do
+    if String.length(head) > _max_size(tail, max),
+      do: String.length(head),
+      else: _max_size(tail, max)
+  end
+
+  defp _calc_trail(head_len, size),
+    do: div(size - head_len, 2) + rem(size - head_len, 2) + head_len
 end
 
 IO.puts(Table.center(["cat", "zebra", "elephant"]))
+
+IO.puts(
+  Table.center([
+    "Chapter 1",
+    "",
+    "C'era una volta",
+    "tanto tanto tempo fa",
+    "uno sviluppatore molto molto felice.",
+    "Elixir era il top!"
+  ])
+)
---
+++ lib/chapter-11/strings-and-binaries-6.ex
@@ -0,0 +1,41 @@
+# elixir lib/chapter-11/strings-and-binaries-6.ex
+
+defmodule CapSen do
+  def capitalize_sentence(str), do: _reverse(_cap(str, [], true))
+
+  defp _cap(<<head::utf8, tail::binary>>, acc, _) when head == ?. do
+    _cap(tail, [head | acc], true)
+  end
+
+  defp _cap(<<head::utf8, tail::binary>>, acc, cap) when head == ?\s do
+    _cap(tail, [head | acc], cap)
+  end
+
+  defp _cap(<<head::utf8, tail::binary>>, acc, true) do
+    _cap(tail, [up(head) | acc], false)
+  end
+
+  defp _cap(<<head::utf8, tail::binary>>, acc, _) do
+    _cap(tail, [down(head) | acc], false)
+  end
+
+  defp _cap(<<>>, acc, _), do: acc
+
+  # Approach 1
+  defp up(char) when char <= ?z and char >= ?a, do: char - 32
+  defp up(char) when char <= ?Z and char >= ?A, do: char
+  defp up(char), do: raise("Invalid character #{char}") # or return original one
+
+  defp down(char) when char <= ?z and char >= ?a, do: char
+  defp down(char) when char <= ?Z and char >= ?A, do: char + 32
+  defp down(char), do: raise("Invalid character #{char}") # or return original one
+
+  defp _reverse(sequence), do: _reverse(sequence, [])
+  defp _reverse([], reversed), do: reversed
+  defp _reverse([head | tail], reversed), do: _reverse(tail, [head | reversed])
+
+  # Approach 2: String.codepoints, 
+end
+
+# IO.puts(CapSen.capitalize_sentence("oh. a DOG. woof. "))
+# "Oh. A dog. Woof. "
